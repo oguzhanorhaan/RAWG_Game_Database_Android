858cf05d1b6ae6ade4749f83c637cdb3849e48ca
Oguzhan <ozy.orhan@gmail.com>
2022-02-21 20:16:41 +0300

    RAWG-04 - Add search and save to db uses cases

---
+++ app/src/main/java/com/oguzhanorhan/rawggamedatabaseandroid/domain/usecase/RetrieveGameListAndSaveToLocaleUseCase.kt
@@ -0,0 +1,30 @@
+package com.oguzhanorhan.rawggamedatabaseandroid.domain.usecase
+
+import com.oguzhanorhan.rawggamedatabaseandroid.data.model.Resource
+import com.oguzhanorhan.rawggamedatabaseandroid.datasource.local.GameEntity
+import com.oguzhanorhan.rawggamedatabaseandroid.datasource.local.RawgLocalRepository
+import com.oguzhanorhan.rawggamedatabaseandroid.datasource.model.Games
+import com.oguzhanorhan.rawggamedatabaseandroid.domain.repository.RawgRepository
+
+class RetrieveGameListAndSaveToLocaleUseCase(
+    private val rawgRepository: RawgRepository,
+    private val rawgLocaleRepository: RawgLocalRepository
+) {
+
+    suspend fun get(apiKey: String): Resource<Games> =
+        rawgRepository.getGameList(apiKey).apply {
+            this.data?.let {
+                it.results?.forEach { gameResponse ->
+                    rawgLocaleRepository.insertGame(
+                        GameEntity(
+                            id = gameResponse.id,
+                            name = gameResponse.name,
+                            rating = gameResponse.rating,
+                            released = gameResponse.formattedDate,
+                            background = gameResponse.background_image
+                        )
+                    )
+                }
+            }
+        }
+}

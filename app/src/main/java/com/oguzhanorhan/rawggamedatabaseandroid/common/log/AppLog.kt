a8a3f7e0c5fcc17a4dbee44c8eb70e9ea09427ea
Oguzhan <ozy.orhan@gmail.com>
2022-02-23 14:40:45 +0300

    RAWG-06 - Add App logger structure

--- app/src/main/java/com/oguzhanorhan/rawggamedatabaseandroid/common/log/AppLog.kt
+++ app/src/main/java/com/oguzhanorhan/rawggamedatabaseandroid/common/log/AppLog.kt
@@ -1,4 +1,21 @@
 package com.oguzhanorhan.rawggamedatabaseandroid.common.log
 
-class AppLog {
+import android.content.Context
+import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+
+object AppLog {
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
+    fun initFirebase(context: Context) {
+        firebaseAnalytics = FirebaseAnalytics.getInstance(context)
+    }
+
+    fun sendEvent(appEventType: AppEventType) {
+
+        firebaseAnalytics.logEvent(appEventType.javaClass.name, Bundle().apply {
+            this.putString(appEventType.key, appEventType.param)
+        })
+
+    }
 }
